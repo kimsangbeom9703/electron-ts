b70836776d4bfb020137ea3937d62fbda39379f7
kimsangbeom9703 <kp41589272@gmail.com>
2023-12-06 10:35:51 +0900

    electron ts success

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,27 @@ import * as path from "path";
 function createWindow() {
     // Create the browser window.
     const mainWindow: BrowserWindow = new BrowserWindow({
+        width: 800,
         height: 1600,
         webPreferences: {
-            preload: path.join(__dirname, "preload.js"),
+            nodeIntegration: true,
+            contextIsolation: false, // contextIsolation이 꺼져있어야 CSP가 적용됨
+            sandbox: false, // contextIsolation이 꺼져있어야 sandbox가 꺼짐
+            webSecurity: true, // 필요에 따라 웹 보안 설정을 true로 유지
+            preload: path.join(__dirname, 'preload.js') // 필요에 따라 preload 스크립트 추가
         },
-        width: 800,
     });
-
+    // 보안 정책 설정 - "unsafe-eval" 사용 안 함
+    mainWindow.webContents.session.webRequest.onHeadersReceived((details, callback) => {
+        callback({
+            responseHeaders: {
+                ...details.responseHeaders,
+                'Content-Security-Policy': ["script-src 'self'"] // 여기서 'unsafe-eval'을 제거하고 적절한 정책으로 대체
+            }
+        });
+    });
     // and load the index.html of the app.
-    mainWindow.loadFile(path.join(__dirname, "../index.html"));
+    mainWindow.loadFile(path.join(__dirname, "../views/index.html"));
 
     // Open the DevTools.
     mainWindow.webContents.openDevTools();
